7c5426a1aa34cddfd94efa067393698079691f3a
test <test@qq.com>
2022-04-20 18:05:35 +0800

    优化购买时库存的变化、备份数据库

--- src/main/java/com/mrxu/stucomplarear2/service/impl/MarketOrderServiceImpl.java
+++ src/main/java/com/mrxu/stucomplarear2/service/impl/MarketOrderServiceImpl.java
@@ -21,7 +21,7 @@ import java.util.concurrent.RecursiveTask;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author Mr.Xu
@@ -39,34 +39,37 @@ public class MarketOrderServiceImpl extends ServiceImpl<MarketOrderMapper, Marke
 
     @Override
     public Result addOrder(OrderAddDto orderAddDto, HttpServletRequest request) {
-        MarketOrderVo marketOrderVo=new MarketOrderVo();
+        MarketOrderVo marketOrderVo = new MarketOrderVo();
         try {
             String accessToken = request.getHeader("Authorization");
             //获取token里面的用户ID
             String userId = JWTUtil.getUserId(accessToken);
-            if (orderAddDto.getGoodsId()==null||orderAddDto.getBuyCount()==null||userId==null) {
+            if (orderAddDto.getGoodsId() == null || orderAddDto.getBuyCount() == null || userId == null) {
                 return Result.fail("参数错误");
             }
 
             //检查商品是否存在
-            Goods goods=goodsMapper.selectById(orderAddDto.getGoodsId());
-            if(goods==null){
+            Goods goods = goodsMapper.selectById(orderAddDto.getGoodsId());
+            if (goods == null) {
                 return Result.fail("商品不存在或已被下架");
             }
-            MarketOrder marketOrder=new MarketOrder();
+            if (goods.getGoodsCount() < 1) {
+                return Result.fail("库存不足");
+            }
+            MarketOrder marketOrder = new MarketOrder();
             marketOrder.setGoodsId(orderAddDto.getGoodsId());
             marketOrder.setBuyerId(Integer.valueOf(userId));
             marketOrder.setSellerId(goods.getUserId());
             marketOrder.setBuyCount(orderAddDto.getBuyCount());
-            marketOrder.setTotalPrice(goods.getGoodsPrice()*orderAddDto.getBuyCount());
+            marketOrder.setTotalPrice(goods.getGoodsPrice() * orderAddDto.getBuyCount());
             marketOrder.setOrderStatus(0);
             marketOrderMapper.insert(marketOrder);
 //            System.out.println("id:"+marketOrder.getOrderId());
             //实体类中@TableName @TableId(value = "order_id", type = IdType.AUTO)
             //一一对应后，inster()之后，实例中可以获取到id
 
-            MarketOrder marketOrder1=marketOrderMapper.selectById(marketOrder.getOrderId());
-            BeanUtils.copyProperties(marketOrder1,marketOrderVo);
+            MarketOrder marketOrder1 = marketOrderMapper.selectById(marketOrder.getOrderId());
+            BeanUtils.copyProperties(marketOrder1, marketOrderVo);
 
             //查对应的用户信息
             User buyer = userMapper.selectById(marketOrder1.getBuyerId());
@@ -85,16 +88,25 @@ public class MarketOrderServiceImpl extends ServiceImpl<MarketOrderMapper, Marke
     @Override
     public Result payOrder(Integer orderId) {
         try {
-            if(orderId==null){
+            if (orderId == null) {
                 return Result.fail("参数错误");
             }
-            MarketOrder marketOrder=marketOrderMapper.selectById(orderId);
-            if(marketOrder==null){
+            MarketOrder marketOrder = marketOrderMapper.selectById(orderId);
+            if (marketOrder == null) {
                 return Result.fail("订单不存在");
             }
+            Goods goods = goodsMapper.selectById(marketOrder.getGoodsId());
+            if (goods.getGoodsCount() < 1) {
+                return Result.fail("库存不足");
+            }
+            //库存减一
+            goods.setGoodsCount(goods.getGoodsCount() - 1);
+            goodsMapper.updateById(goods);
+
+            //更改订单状态为“已支付”
             marketOrder.setOrderStatus(1);
             marketOrderMapper.updateById(marketOrder);
-        }catch (Exception e){
+        } catch (Exception e) {
             return Result.fail(e.toString());
         }
         return Result.succ("支付成功");
